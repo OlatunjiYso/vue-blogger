0a1013c7af8007425626be6dac1542ff30048097
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-02-16 12:46:26 +0100

    add router file

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import HomeComponent from './components/HomeComponent';
+import CreateComponent from './components/CreateComponent';
+import EditComponent from './components/EditComponent';
+
+
+
+export const routes = [
+  {name: 'home',
+  path: '/posts',
+  component: HomeComponent
+},
+{
+  name: 'create',
+  path: '/create',
+  component: CreateComponent
+},
+{
+  name: 'edit',
+  path: '/edit/:id',
+  component: EditComponent
+}
+]
