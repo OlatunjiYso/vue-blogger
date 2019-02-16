84d7ad12f84f0be96ddf98f2e92ca50d02c8585d
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-02-16 12:44:13 +0100

    add footer component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="footer-component">
+    <div class="footer-item">
+      <h5>Aspects</h5>
+      <ul>
+        <li>Blogging</li>
+        <li>Life</li>
+        <li>Support</li>
+        <li>Manage</li>
+      </ul>
+    </div>
+    <div class="footer-item">
+      <h5>Address</h5>
+      <ul>
+        <li> No 345 azxt </li>
+        <li> Ilupeju </li>
+        <li> Anthony Arena </li>
+        <li> Lagos state/ </li>
+      </ul>
+    </div>
+     <div class="footer-item">
+      <h5>Aspects</h5>
+      <ul>
+        <li>Blogging</li>
+        <li>Life</li>
+        <li>Support</li>
+        <li>Manage</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#footer-component {
+  display: flex;
+  justify-content: flex-start;
+  background-color: rgb(7, 14, 8);
+  height: 25%;
+  color: rgb(189, 194, 190);
+}
+
+h5 {
+  padding: 2px 10px;
+  background-color: rgb(197, 196, 196);
+  color: rgb(7, 14, 8);
+  margin: 10px 40px;
+}
+li {
+  list-style-type: none
+}
+.footer-item{
+  padding: 20px 0;
+  margin-right: 30%;
+}
+</style>
+
