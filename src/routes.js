e2b37594cbef99c04f54175b1a3249cc5c62c6c7
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-02-21 20:34:42 +0100

    add routes for single blog component

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,19 @@
 import HomeComponent from './components/HomeComponent';
 import CreateComponent from './components/CreateComponent';
 import EditComponent from './components/EditComponent';
-
+import BlogComponent from './components/BlogComponent';
 
 
 export const routes = [
-  {name: 'home',
-  path: '/posts',
+{
+  name: 'home',
+  path: '/blogposts',
   component: HomeComponent
+},
+{
+  name: 'blog',
+  path: '/blogpost/:id',
+  component: BlogComponent
 },
 {
   name: 'create',
